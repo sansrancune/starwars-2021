88aec36848cf1d494e7a3e50c1cb1bfcb3c77776
Yannick <yannick.boogaerts@technifutur.be>
2021-04-16 13:32:35 +0200

    version initiale

---
+++ src/be/technifutur/starwars/Scenario1.java
@@ -0,0 +1,8 @@
+package be.technifutur.starwars;
+
+public class Scenario1 {
+
+    public static void main(String[] args) {
+        System.out.println("Il y a très très longtemps dans une galaxies très très lointaine ...");
+    }
+}
